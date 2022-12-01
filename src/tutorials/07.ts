b9c63e4655d1b2c42ba480272f15666d2da89c0e
Michał Kukla <degregar@gmail.com>
2022-12-01 12:17:07 +0100

    chore: remove new lines from json message

--- src/tutorials/07.ts
+++ src/tutorials/07.ts
@@ -4,7 +4,7 @@ import { SuccessJsonResponseDto } from "../types"
 export const tutorial7Handler = (req: Request, res: Response) => {
   const data: SuccessJsonResponseDto = {
     title: "Tutorial 7",
-    message: "Now you know the basics of REST API and using Postman.\n\nLet's start a game!",
+    message: "Now you know the basics of REST API and using Postman. Let's start a game!",
     prevPage: "/tutorials/6",
     nextPage: "/riddles",
   }
